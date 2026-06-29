a8146bb49346f28337c5b94c68376edb811ffc57
Limon <skakao1234@naver.com>
2026-06-29 12:56:43 +0900

    glfw추가

--- Dependency.cmake
+++ Dependency.cmake
@@ -9,7 +9,7 @@ set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
 
 # spdlog: fast logger library
 ExternalProject_Add(
-    dep-spdlog
+    dep_spdlog
     GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
     GIT_TAG "v1.x"
     GIT_SHALLOW 1
@@ -19,7 +19,25 @@ ExternalProject_Add(
     #CMAKE_INSTALL_PREFIX cmake가 빌드를 다 했을 때, 어디에 install할지 정하는 것
 
     TEST_COMMAND ""
-)
+    )
 # Dependency 리스트 및 라이브러리 파일 리스트 추가
-set(DEP_LIST ${DEP_LIST} dep-spdlog)
+set(DEP_LIST ${DEP_LIST} dep_spdlog)
 set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>)
+
+# glfw
+ExternalProject_Add(
+    dep_glfw
+    GIT_REPOSITORY "https://github.com/glfw/glfw.git"
+    GIT_TAG "3.3.3"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND "" PATCH_COMMAND "" TEST_COMMAND ""
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+        -DGLFW_BUILD_EXAMPLES=OFF
+        -DGLFW_BUILD_TESTS=OFF
+        -DGLFW_BUILD_DOCS=OFF
+        #핵심: 하위 GLFW 빌드가 최신 CMake 엔진에서도 에러 없이 작동하도록 최저 정책 마지노선을 주입합니다.
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+    )
+set(DEP_LIST ${DEP_LIST} dep_glfw)
+set(DEP_LIBS ${DEP_LIBS} glfw3)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, const char** argv){
-    SPDLOG_INFO("hello, opengl");
+    SPDLOG_INFO("Start program"); // 시작을 알리는 로그
+    // glfw 라이브러리 초기화, 실패하면 에러 출력후 종료
+    SPDLOG_INFO("Initialize glfw");
+    if (!glfwInit()) {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
+    }
+    // glfw 윈도우 생성, 실패하면 에러 출력후 종료
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if (!window) {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+    //glfwWindowShouldClose(window)->윈도우가 닫혀야 하는지 여부를 반환, true이면 루프 종료, false(닫힐 이유가 없다면)이면 계속 루프
+    //glfwPollEvents()->이벤트를 처리, 윈도우가 닫히는 이벤트가 발생하면 glfwWindowShouldClose(window)가 true를 반환하도록 설정
+
+    glfwTerminate();
     return 0;
 }
